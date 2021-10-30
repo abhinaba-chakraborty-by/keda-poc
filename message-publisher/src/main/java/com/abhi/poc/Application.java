1250d86a4fe2d59699adcb5a105f3a4d7a7c4204
Abhinaba Chakraborty <Abhinaba.Chakraborty@blueyonder.com>
2021-10-30 19:06:19 +0530

    Updated message publisher logic

--- message-publisher/src/main/java/com/abhi/poc/Application.java
+++ message-publisher/src/main/java/com/abhi/poc/Application.java
@@ -5,7 +5,7 @@ import com.azure.messaging.servicebus.ServiceBusMessage;
 import com.azure.messaging.servicebus.ServiceBusMessageBatch;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -13,20 +13,31 @@ public class Application {
   private static final String CONNECTION_STRING = "";
   private static final String QUEUE_NAME = "item-ingestion-queue";
 
-  public static void main(String[] args) {
-    sendMessageBatch();
+  public static void main(String[] args) throws InterruptedException {
+    startSendingMessages();
   }
 
-  private static void sendMessageBatch() {
+  private static void startSendingMessages() throws InterruptedException {
+    int msgCounter = 1;
     try (ServiceBusSenderClient senderClient = new ServiceBusClientBuilder().connectionString(CONNECTION_STRING).sender().queueName(QUEUE_NAME).buildClient()) {
+      Thread.sleep(5000);
+      for (; msgCounter <= 100; msgCounter++) {
+        senderClient.sendMessage(new ServiceBusMessage("Message " + msgCounter));
+      }
+      System.out.println("Sent 100 messages one by one to the queue");
+      Thread.sleep(5000);
+
       ServiceBusMessageBatch messageBatch = senderClient.createMessageBatch();
-      List<ServiceBusMessage> listOfMessages = createMessages();
-      for (ServiceBusMessage message : listOfMessages) {
+      List<ServiceBusMessage> listOfNext10000Messages = new ArrayList<>();
+      for (; msgCounter <= 10100; msgCounter++) {
+        listOfNext10000Messages.add(new ServiceBusMessage("Message " + msgCounter));
+      }
+      for (ServiceBusMessage message : listOfNext10000Messages) {
         if (messageBatch.tryAddMessage(message)) {
           continue;
         }
         senderClient.sendMessages(messageBatch);
-        System.out.println("Sent a batch of messages to the queue: " + QUEUE_NAME);
+        System.out.println("Sent " + messageBatch.getCount() + " batch of messages to the queue: " + QUEUE_NAME);
         messageBatch = senderClient.createMessageBatch();
         if (!messageBatch.tryAddMessage(message)) {
           System.err.printf("Message is too large for an empty batch. Skipping. Max size: %s.", messageBatch.getMaxSizeInBytes());
@@ -34,17 +45,8 @@ public class Application {
       }
       if (messageBatch.getCount() > 0) {
         senderClient.sendMessages(messageBatch);
-        System.out.println("Sent a batch of messages to the queue: " + QUEUE_NAME);
+        System.out.println("Sent " + messageBatch.getCount() + " batch of messages to the queue: " + QUEUE_NAME);
       }
     }
-  }
-
-  private static List<ServiceBusMessage> createMessages() {
-    ServiceBusMessage[] messages = {
-        new ServiceBusMessage("First message"),
-        new ServiceBusMessage("Second message"),
-        new ServiceBusMessage("Third message")
-    };
-    return Arrays.asList(messages);
   }
 }
